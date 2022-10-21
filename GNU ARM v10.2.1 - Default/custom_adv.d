72e6348a28644527deb092e48fa68352297af962
Claudio Filho <claudioasfilho@gmail.com>
2022-10-21 15:03:44 -0700

    First commit

---
+++ GNU ARM v10.2.1 - Default/custom_adv.d
@@ -0,0 +1,19 @@
+custom_adv.o: ../custom_adv.c \
+ /Users/clfilho/SimplicityStudio/SDKs/gecko_sdk_2//platform/common/toolchain/inc/sl_gcc_preinclude.h \
+ ../custom_adv.h \
+ /Users/clfilho/SimplicityStudio/SDKs/gecko_sdk_2//protocol/bluetooth/inc/sl_bt_api.h \
+ /Users/clfilho/SimplicityStudio/SDKs/gecko_sdk_2//platform/common/inc/sl_status.h \
+ /Users/clfilho/SimplicityStudio/SDKs/gecko_sdk_2//protocol/bluetooth/inc/sl_bgapi.h \
+ /Users/clfilho/SimplicityStudio/SDKs/gecko_sdk_2//protocol/bluetooth/inc/sl_bt_api_compatibility.h \
+ /Users/clfilho/SimplicityStudio/SDKs/gecko_sdk_2//app/common/util/app_assert/app_assert.h \
+ /Users/clfilho/SimplicityStudio/May_22/AM_beacon_2_TB22/autogen/sl_component_catalog.h \
+ /Users/clfilho/SimplicityStudio/May_22/AM_beacon_2_TB22/config/app_assert_config.h
+/Users/clfilho/SimplicityStudio/SDKs/gecko_sdk_2//platform/common/toolchain/inc/sl_gcc_preinclude.h:
+../custom_adv.h:
+/Users/clfilho/SimplicityStudio/SDKs/gecko_sdk_2//protocol/bluetooth/inc/sl_bt_api.h:
+/Users/clfilho/SimplicityStudio/SDKs/gecko_sdk_2//platform/common/inc/sl_status.h:
+/Users/clfilho/SimplicityStudio/SDKs/gecko_sdk_2//protocol/bluetooth/inc/sl_bgapi.h:
+/Users/clfilho/SimplicityStudio/SDKs/gecko_sdk_2//protocol/bluetooth/inc/sl_bt_api_compatibility.h:
+/Users/clfilho/SimplicityStudio/SDKs/gecko_sdk_2//app/common/util/app_assert/app_assert.h:
+/Users/clfilho/SimplicityStudio/May_22/AM_beacon_2_TB22/autogen/sl_component_catalog.h:
+/Users/clfilho/SimplicityStudio/May_22/AM_beacon_2_TB22/config/app_assert_config.h:
